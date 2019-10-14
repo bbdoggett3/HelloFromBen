8b2624dcb57cb4b5b9f6a7a2bfbf679df87aea83
vcbenj <whi15026@byui.edu>
2019-10-14 09:33:37 -0600

    Project04

--- src/doggett/Main.java
+++ src/doggett/Main.java
@@ -3,6 +3,8 @@ package doggett;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello my name is Ben!");
+        System.out.println("No.. my name is Ben.");
     }
 }
